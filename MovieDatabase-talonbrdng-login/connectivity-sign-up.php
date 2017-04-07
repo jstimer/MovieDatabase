ca2660b94ef2d286f0b096d8acc8c3bad9dc8ea5
crc8962 <crc8962@lhup.edu>
2017-04-07 11:59:41 -0400

    Add files via upload

---
+++ MovieDatabase-talonbrdng-login/connectivity-sign-up.php
@@ -0,0 +1,48 @@
+<?php
+
+   define('DB_SERVER', 'willy');
+   define('DB_USERNAME', 'comp305_grp2');
+   define('DB_PASSWORD', 'Scrumbags1!');
+   define('DB_DATABASE', 'comp305_grp2');
+
+$con=mysql_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE) or die("Failed to connect to MySQL: " . mysql_error());
+$db=mysql_select_db(DB_DATABASE,$con) or die("Failed to connect to MySQL: " . mysql_error());
+
+
+function NewUser()
+{
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	$first_name = $_POST['first_name'];
+	$last_name =  $_POST['last_name'];
+	$email = $_POST['email'];
+	$query = "INSERT INTO users (username,password,first_name,last_name,email) VALUES ('$username','$password','$first_name','$last_name', '$email')";
+	$data = mysql_query ($query)or die(mysql_error());
+	if($data)
+	{
+	echo "YOUR REGISTRATION IS COMPLETED...";
+	}
+}
+
+function SignUp()
+{
+if(!empty($_POST['username']))   //checking the 'user' name which is from Sign-Up.html, is it empty or have some text
+{
+	$query = mysql_query("SELECT * FROM users WHERE username = '$_POST[username]' AND password = '$_POST[password]'") or die(mysql_error());
+
+	if(!$row = mysql_fetch_array($query) or die(mysql_error()))
+	{
+		NewUser();
+	}
+	else
+	{
+		echo "SORRY...YOU ARE ALREADY REGISTERED USER...";
+	}
+}
+}
+if(isset($_POST['submit']))
+{
+	SignUp();
+}
+?>
+
