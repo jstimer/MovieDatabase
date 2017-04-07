d213b9a4bc76476f1f93c45d9d568615210e1537
anm3576 <adammackie08@gmail.com>
2017-04-07 11:14:15 -0400

    Add files via upload

--- contact_us.php
+++ contact_us.php
@@ -10,20 +10,43 @@
 		
 		<!-- webpage info should go here-->
 		<div class="body"> 
-			<p> CONTACT US
- 			Included on this page is a list of administrators who are available to 				receive any questions or comments that members may have. We are a 24/7 				customer support team for the website. 
-			
+			<h5> CONTACT US </h5>
+			<br>
+ 			Included on this page is a list of administrators who are available to 				
+ 			receive any questions or comments that members may have. We are a 24/7 				
+ 			customer support team for the website. 
+			<br>
+			<br>
 			Account issues
-			- Issues involving account registration or login
-			
+			<ul>
+				<li> Issues involving account registration or login</li>
+								
+				</ul>
+			<br>		
 			Business operations
-			- Advertising available on website
-			- Inaccurate data on webpage
-			- User submissions of movies to administrator 
-
-
-
-			Members will have the opportunity to leave feedback on open forums 				moderated by adminstrators to see common problems that other users are 				having and be able to discuss movies or shows that they want to see on our 				website.
+			<ul>
+				<li> Advertising available on website</li>
+				<li> Inaccurate data on webpage</li>
+				<li> User submissions of movies to administrator</li> 
+				</ul>
+				
+			Members will have the opportunity to leave feedback on open forums 				
+			moderated by adminstrators to see common problems that other users are 				
+			having and be able to discuss movies or shows that they want to see on our 				
+			website.
+			
+			Listed below is administrators on call to answer questions you may have:
+			<ul> 
+				<li> Adam Mackie			-	Anm3576@lhup.edu</li>
+				<li> Matthew Schueler 	-	Mrs7181@lhup.edu</li>
+				<li> Jerry Stimer			-	Jas1835@lhup.edu</li>
+				<li> Casey Capwell		-	Crc8962@lhup.edu</li>
+				<li> Joshua Spencer		-	Jas2986@lhup.edu</li>
+				<li> Brandon Singer		-	Bws7126@lhup.edu</li>
+				<li> Cody Welch			-	Crw6730@lhup.edu</li>
+				<li> Kianna Patterson	-	Kap7849@lhup.edu</li>
+				</ul>
+				
 </p>
 			
 		</div>
