ddf167701a261e61684c405c6fd9baeb6c8c2e85
mSchul4 <matt.schueler43@gmail.com>
2017-04-30 19:28:23 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php require "movie_db.inc";
-
 	StartHTML( "Movie Database" );
 	
 	HTMLHeader();
@@ -11,23 +10,21 @@
 		
 		
 		<!-- webpage info should go here-->
-		<div class="body">
-			<!DOCTYPE html>
-<html>
-<head>
+
+
+		
+
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
 * {box-sizing:border-box}
 body {font-family: Verdana,sans-serif;margin:0}
 .mySlides {display:none}
-
 /* Slideshow container */
 .slideshow-container {
-  max-width: 1000px;
+  max-width: 500px;
   position: relative;
   margin: auto;
 }
-
 /* Next & previous buttons */
 .prev, .next {
   cursor: pointer;
@@ -40,20 +37,17 @@ body {font-family: Verdana,sans-serif;margin:0}
   font-weight: bold;
   font-size: 18px;
   transition: 0.6s ease;
-  border-radius: 0 3px 3px 0;
+  border-radius: 0 3px 3px 0;http://10.1.2.2/~mrs7181/305php/MovieDB-Sprint3/movies.php
 }
-
 /* Position the "next button" to the right */
 .next {
   right: 0;
   border-radius: 3px 0 0 3px;
 }
-
 /* On hover, add a black background color with a little bit see-through */
 .prev:hover, .next:hover {
   background-color: rgba(0,0,0,0.8);
 }
-
 /* Caption text */
 .text {
   color: #f2f2f2;
@@ -62,9 +56,8 @@ body {font-family: Verdana,sans-serif;margin:0}
   position: absolute;
   bottom: 8px;
   width: 100%;
-  text-align: center;
+  text-align: left;
 }
-
 /* Number text (1/3 etc) */
 .numbertext {
   color: #f2f2f2;
@@ -73,7 +66,6 @@ body {font-family: Verdana,sans-serif;margin:0}
   position: absolute;
   top: 0;
 }
-
 /* The dots/bullets/indicators */
 .dot {
   cursor:pointer;
@@ -85,55 +77,51 @@ body {font-family: Verdana,sans-serif;margin:0}
   display: inline-block;
   transition: background-color 0.6s ease;
 }
-
 .active, .dot:hover {
   background-color: #717171;
 }
-
 /* Fading animation */
 .fade {
   -webkit-animation-name: fade;
   -webkit-animation-duration: 1.5s;
-  animation-name: fade;
+  animation-name: fade;http://10.1.2.2/~mrs7181/mrs7181/305php/MovieDB-Sprint3/movies.php
   animation-duration: 1.5s;
 }
-
 @-webkit-keyframes fade {
   from {opacity: .4} 
   to {opacity: 1}
 }
-
 @keyframes fade {
   from {opacity: .4} 
   to {opacity: 1}
 }
-
 /* On smaller screens, decrease text size */
 @media only screen and (max-width: 300px) {
   .prev, .next,.text {font-size: 11px}
 }
 </style>
-</head>
-<body>
+
+<div class="body">
+<p><b> Top videos this week! </b></p>
 
 <div class="slideshow-container">
 
 <div class="mySlides fade">
   <div class="numbertext">1 / 3</div>
-  <img src="kingkong.jpg" style="width:100%">
-  <div class="text">Caption Text</div>
+  <img src="kongpbanner3.jpg "width="500px" height ="200px">
+  <div class="text"></div>
 </div>
 
 <div class="mySlides fade">
   <div class="numbertext">2 / 3</div>
-  <img src="darkknight.jpg" style="width:100%">
-  <div class="text">Caption Two</div>
+  <img src="darkknight.jpg "width="500px" height ="200">
+  <div class="text"></div>
 </div>
 
 <div class="mySlides fade">
   <div class="numbertext">3 / 3</div>
-  <img src="sausageparty.jpg" style="width:100%">
-  <div class="text">Caption Three</div>
+  <img src="sausageparty.jpg "width="500px" height ="200">
+  <div class="text"></div>
 </div>
 
 <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
@@ -151,15 +139,12 @@ body {font-family: Verdana,sans-serif;margin:0}
 <script>
 var slideIndex = 1;
 showSlides(slideIndex);
-
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
-
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
-
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
@@ -176,13 +161,128 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 </script>
+<!-- Top Rated Movies -->
+<p><b>Top Rated Movies</b></p>
+
+<p>
+ <a href="single.php?filmName=881661">
+<img border="0" alt="Guardians of the Galaxy" src="guardians.jpg" width="100" height="100">
+</a>
+
+ <a href="single.php?filmName=881641">
+<img border="0" alt="The 40-Year-Old Virgin" src="40yr.jpg" width="100" height="100">
+</a>
+
+ <a href="single.php?filmName=881616">
+<img border="0" alt="2 Fast 2 Furious" src="2fast.jpg" width="100" height="100">
+</a>
+
+ <a href="single.php?filmName=881617">
+<img border="0" alt="Back to the Future" src="back2.jpg" width="100" height="100">
+</a>
+
+ <a href="single.php?filmName=881652">
+<img border="0" alt="Knocked Up" src="knocked.jpg" width="100" height="100">
+</a>
+			<!-- Welcome message -->
+<p><b>Welcome to our Movie Website! Enjoy free trailers and information about<br> 
+new movies, older movies or even movies that haven't been shown yet! </b></p>
+
+
+<!-- hover images -->
+<style>
+.container {
+  position: relative;
+  width: 50%;
+  white-space: nowrap;
+}
+.image {
+  display: block;
+  width: 500px;
+  height: auto;
+  
+}
+.overlay {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
+  width: 500px;
+  opacity: 0;
+  transition: .5s ease;
+  background-color: #008CBA;
+}
+.container:hover .overlay {
+  opacity: 1;
+}
+.text {
+  color: white;
+  font-size: 20px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+}
+</style>
+</head>
+<body>
+
+
+<p><b>Upcoming Movies!</b></p>
+
+<div class="container">
+  <img  src="transformers.jpg" alt="Transformers: The Last Knight" width="250" height="350" class="image">
+  <div class="overlay">
+    <div class="text">Release Date: June 23, 2017</div>
+  </div>
+  </div>
+
+<p>
+ Humans and Transformers are at war, Optimus Prime is gone.<br>
+ The key to saving our future lies buried in the secrets of the past,<br> 
+ in the hidden history of Transformers on Earth.
+  </p>
+
+<div class="container">
+  <img  src="mummy.jpg" alt="The Mummy (2017)" width="250" height="350" class="image">
+  <div class="overlay">
+    <div class="text">Release Date: June 9, 2017</div>
+  </div>
+</div>
+
+<p>An ancient princess is awakened from her crypt beneath the desert,<br>
+ bringing with her malevolence grown over millennia, and terrors that<br>
+  defy human comprehension.
+</p>
+
+<div class="container">
+  <img src="despicable.jpg" alt="Despicable Me 3" width="250" height="350" class="image">
+  <div class="overlay">
+    <div class="text">Release Date: June 30, 2017</div>
+  </div>
+</div>
+
+<p>Gru meets his twin brother Dru he never knew about.</p>
+
+<div class="container">
+  <img src="spider.jpg" alt="Spider-Man: Homecoming" width="250" height="350" class="image">
+  <div class="overlay">
+    <div class="text">Release Date: July 5, 2017</div>
+  </div>
+</div>
+
+<p>Following the events of Captain America: Civil War (2016),<br> 
+Peter Parker attempts to balance his life in high school with his<br>
+ career as the web-slinging superhero Spider-Man.
 
 </body>
-</html> 
 
-		</div>
-		
-		
+
+
+
 
 <?php	
 		HTMLFooter();
